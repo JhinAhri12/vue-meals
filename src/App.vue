053731dc648ed92e1dc0f4a6a318929926e74998
JhinAhri12 <cyprien.rezki@hotmail.fr>
2022-09-05 11:42:56 +0200

    add axios and call api

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script setup>
-
+import Meal from "@/components/Meal.vue"
+import MealService from "@/services/MealService.js";
 </script>
 
 <template>
-
-
   <main>
-   
+    <Meal/>
   </main>
 </template>
 
---
+++ src/components/Meal.vue
@@ -0,0 +1,28 @@
+<template>
+    mon component
+    {{meal}}
+</template>
+
+<script>
+import { watchEffect } from "vue";
+import MealService from "@/services/MealService.js";
+export default {
+    data() {
+        return {
+        meal: null
+        };
+  },
+    created() {
+        watchEffect(() => {
+        this.meal = null;
+        MealService.getRandomMeal()
+        .then((response) => {
+          this.meal = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    });
+  },
+}
+</script>
